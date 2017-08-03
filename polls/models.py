e0301a93e6db7979ee4eabea8fac4f1a7ce7989b
ILeftHogwarts <liken2134@gmail.com>
2017-08-03 13:35:27 +0300

    models changes

--- polls/models.py
+++ polls/models.py
@@ -2,18 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+
+class CustomerRecord(models.Model):
+    name = models.CharField(max_length = 100)
+    phone = models.CharField(max_length = 15, primary_key = True)
+    add_phone = models.IntegerField()
+    adress = models.CharField(max_length = 150)
+
+
 class CustomerOrders(models.Model):
-    phone = models.IntegerField(primary_key = True)
+    id  = models.AutoField(primary_key = True)
+    phone = models.ForeignKey(CustomerRecord, on_delete=models.CASCADE)
     preformance = models.CharField(max_length = 250)
     places = models.CharField(max_length = 50)
     price = models.CharField(max_length = 50)
-
-class CustomerRecord(models.Model):
-    id = models.AutoField(primary_key = True)
-    name = models.CharField(max_length = 100)
-    phone = models.ForeignKey(CustomerOrders, on_delete = models.CASCADE)
-    add_phone = models.IntegerField()
-    adress = models.CharField(max_length = 150)
 
 class TheatreInfo(models.Model):
     id = models.AutoField(primary_key = True)
@@ -23,3 +25,4 @@ class TheatreInfo(models.Model):
 
     def __str__(self):
         return self.preformance + ' ' + self.places
+
