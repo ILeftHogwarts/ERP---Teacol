412bcf386bfaa856e872a7098671977cc0650513
ILeftHogwarts <liken2134@gmail.com>
2017-08-09 22:27:51 +0300

    Simple view of erp

--- ERPTeacol/polls/models.py
+++ ERPTeacol/polls/models.py
@@ -2,17 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class CustomerOrders(models.Model):
-    phone = models.IntegerField(primary_key = True)
-    preformance = models.CharField(max_length = 250)
-    places = models.CharField(max_length = 50)
-    price = models.CharField(max_length = 50)
 
 class CustomerRecord(models.Model):
-    id = models.AutoField(primary_key = True)
     name = models.CharField(max_length = 100)
-    phone = models.ForeignKey(CustomerOrders, on_delete = models.CASCADE)
-    add_phone = models.IntegerField()
+    phone = models.CharField(max_length = 15, primary_key = True)
+    add_phone = models.CharField(max_length = 15)
     adress = models.CharField(max_length = 150)
 
 class TheatreInfo(models.Model):
@@ -20,3 +14,14 @@ class TheatreInfo(models.Model):
     preformance = models.CharField(max_length = 250)
     places = models.CharField(max_length = 50)
     price = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.preformance + ' ' + self.places
+
+class CustomerOrders(models.Model):
+    id  = models.AutoField(primary_key = True)
+    phone = models.ForeignKey(CustomerRecord, on_delete=models.CASCADE)
+    theatre_info = models.ForeignKey(TheatreInfo, on_delete=None)
+
+
+
--- ERPTeacol/polls/views.py
+++ ERPTeacol/polls/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.template import RequestContext, loader
 from django.http import HttpResponse
+from .models import TheatreInfo
+from django.views import generic
+
+from .models import CustomerRecord, TheatreInfo
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Hello world!')
+
+
+class WorkPageView(generic.ListView):
+    template_name = 'polls/work_page.html'
+    context_object_name = 'clients_records_list'
+    queryset = CustomerRecord.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super(WorkPageView,self).get_context_data(**kwargs)
+        context['theatre_info_list'] = TheatreInfo.objects.all()
+        return context
+
